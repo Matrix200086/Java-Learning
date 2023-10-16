f30faf22a04d2387c1f6f0da29f5de016e3a146d
Matrix200086 <oyebamijimohammed2020@gmail.com>
2023-10-16 17:19:48 -0400

    update on jagged Arrays

--- Hello.java
+++ Hello.java
@@ -15,12 +15,20 @@
 
 public class Hello {
     public static void main(String  a[] ) {
-        int nums[][] = new int[3][4];
-        for(int i=0; i<3;i++)
+        int nums[][] = new int[3][]; // Jagged Arrays  
+          nums[0]= new int[3];
+          nums[1] = new int[4];
+          nums[2] = new int[2];
+         
+        for(int i=0; i<nums.length;i++)
         {
-            for(int j=0;j<4;j++){
-                nums[i][j] =(int)(Math.random()* 100);
-                System.out.print(nums[i][j] + " ");
+            for(int j=0;j<nums[i].length;j++){
+            nums[i][j] =(int)(Math.random()* 100);   
+        }
+    }
+        for (int n[]: nums){
+            for(int m:n){
+                System.out.print(m + " ");
             }
             System.out.println();
         }
